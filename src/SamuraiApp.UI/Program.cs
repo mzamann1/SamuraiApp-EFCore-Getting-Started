a5c2b32532930c1be89499befa1623a327cecfaf
DELL-7480-PC\Zaman <mzamann1@users.noreply.github.com>
2022-11-16 12:15:50 +0500

    Add project files.

---
+++ src/SamuraiApp.UI/Program.cs
@@ -0,0 +1,32 @@
+﻿using SamuraiApp.Domain;
+using SamuraiApp.Infrastructure;
+
+namespace SamuraiApp.UI;
+public class Program
+{
+    private static SamuraiContext _context = new SamuraiContext();
+
+    private static void Main(string[] args)
+    {
+        _context.Database.EnsureCreated();
+        GetSamurais("Before Add:");
+        AddSamurai();
+        GetSamurais("After Add:");
+        Console.WriteLine("Press any key...");
+        Console.ReadLine();
+    }
+
+    private static void AddSamurai()
+    {
+        var samurai = new Samurai { Name = "Julie" };
+        _context.Samurais.Add(samurai);
+        _context.SaveChanges();
+    }
+    private static void GetSamurais(string text)
+    {
+        var samurais = _context.Samurais.ToList();
+        Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+        Console.WriteLine(string.Join(" , ", samurais.Select(x => x.Name)));
+
+    }
+}
